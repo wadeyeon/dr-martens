7186a3b23a6d2dfb74dcfdc997d70354c8e0a576
wadeyeon <wadeyeon@gmail.com>
2025-03-18 19:57:42 +0900

    Update: Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { BsFillCartFill, BsFillPencilFill } from 'react-icons/bs';
+import { Link } from 'react-router';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className='flex justify-between items-center p-8 border-b'>
+      <Link to='/'>
+        <img className='w-32' src='/images/logo.svg' alt='logo' />
+      </Link>
+      <nav className='flex items-center gap-4'>
+        <Link to='/products'>Products</Link>
+        <Link to='/products'>
+          <BsFillPencilFill />
+        </Link>
+        <Link to='/carts'>
+          <BsFillCartFill />
+        </Link>
+        <button>LogIn</button>
+      </nav>
+    </header>
+  );
 }

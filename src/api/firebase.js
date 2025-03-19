0bb850e14c00354a0f37655b4cccc482b7b190fa
wadeyeon <wadeyeon@gmail.com>
2025-03-19 16:52:03 +0900

    Add: adminUser function
    admin 여부를 확인할 수 있는 속성이 추가된 user 반환

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,15 +6,18 @@ import {
   signInWithPopup,
   signOut,
 } from 'firebase/auth';
+import { get, getDatabase, ref } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 const provider = new GoogleAuthProvider();
 
 export function login() {
@@ -36,9 +39,23 @@ export function logout() {
 export function onUserStateChange(callback) {
   // login(), logout() 호출되면 user 정보가 바뀐다.
   // user 상태가 바뀔때마다 onAuthStateChanged 메소드의 두번째 파라미터의 콜백함수인 관찰자 함수가 실행됨!
-  onAuthStateChanged(auth, (user) => {
+  onAuthStateChanged(auth, async (user) => {
     // Navbar 컴포넌트가 마운트시 useEffect 훅의 콜백함수가 호출되어
     // onUserStateChange(setUser)를 통해 setUser를 콜백함수로 전달됨.
-    callback(user); // setUser(user) 실행! -> login(), logout() 함수에서 user 정보를 리턴할 필요 없어짐.
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser); // setUser(user) 실행! -> login(), logout() 함수에서 user 정보를 리턴할 필요 없어짐.
   });
+}
+
+export async function adminUser(user) {
+  try {
+    const snapshot = await get(ref(database, 'admins'));
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      return { ...user, isAdmin: admins.includes(user.uid) };
+    }
+    return user;
+  } catch (error) {
+    console.error(error.message);
+  }
 }

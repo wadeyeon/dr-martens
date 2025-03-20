3672655f8c12b8b13751254f7b7a297ef091321b
wadeyeon <wadeyeon@gmail.com>
2025-03-20 21:09:00 +0900

    Add: ProductDetail component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
-export default function ProductCard({ product: { image, title, price } }) {
+export default function ProductCard({
+  product,
+  product: { image, title, price },
+}) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`products/${product.id}`, { state: { product } });
+  };
+
   return (
-    <li className='rounded-lg overflow-hidden shadow-md cursor-pointer'>
+    <li
+      className='rounded-lg overflow-hidden shadow-md cursor-pointer'
+      onClick={handleClick}
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='p-2'>
         <h3>{title}</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import Button from '../components/ui/Button';
+import { useLocation } from 'react-router';
 
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const {
+    state: {
+      product: { title, image, price, description, category, size },
+    },
+  } = useLocation();
+
+  return (
+    <>
+      <p className='mt-4 mx-12 text-gray-700'>{category}</p>
+      <section className='flex flex-col md:flex-row p-4'>
+        <img
+          className='basis-5/12 px-4 w-full max-w-xl'
+          src={image}
+          alt={title}
+        />
+        <div className='basis-7/12 flex flex-col w-full p-4'>
+          <h2 className='text-3xl font-bold'>{title}</h2>
+          <p className='py-2 text-gray-500'>{description}</p>
+          <p className='pb-2'>&#8361; {price.toLocaleString()}</p>
+          <select className='my-2 py-2 outline-none border'>
+            {size &&
+              size.map((value, index) => <option key={index}>{value}</option>)}
+          </select>
+          <Button text='장바구니 추가' />
+        </div>
+      </section>
+    </>
+  );
 }
